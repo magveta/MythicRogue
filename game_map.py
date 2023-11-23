95c64f3f1df5a8d20d0b66f07e4bc59fca73ef5f
magveta <kaminari061@gmail.com>
2023-11-22 22:35:11 -0300

    Minor corrections

--- game_map.py
+++ game_map.py
@@ -25,6 +25,10 @@ class GameMap:
             (width, height), fill_value=False, order="F"
         )  # Tiles the player has seen before
     @property
+    def gamemap(self) -> GameMap:
+        return self
+        
+    @property
     def actors(self) -> Iterator[Actor]:
         """Iterate over this maps living actors."""
         yield from (
